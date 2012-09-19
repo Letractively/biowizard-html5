210fe58f6d37d38f2f6bec0146222eb274d18474
mar86.cvl@gmail.com <mar86.cvl@gmail.com@4970fd07-a80c-aea7-95ce-73c01088be99>
2012-09-19 17:06:25 +0000

    adding edit dictionary panel

---
+++ trunk/editPanel.php
@@ -0,0 +1,13 @@
+
+<div id="editDictionary" class="respanel" style="display:none">
+<table>
+<tr>
+<td width="23px">
+</td>
+<td width="100px">Id</td><td id="titlelabel">Title</td></tr>
+</table>
+<div style="overflow:auto; overflow-x:auto; height:420px">
+<table id="editTable" cellpadding="2px" >
+</table>
+</div>
+</div>
