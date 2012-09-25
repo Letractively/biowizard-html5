6ce871615da7f8aabbba1b933c7b6c1f92a92c17
polito.carmelo.88@gmail.com <polito.carmelo.88@gmail.com@4970fd07-a80c-aea7-95ce-73c01088be99>
2012-09-25 13:35:42 +0000

    fixed some bugs
    TODO: completing clustering inspector module

--- trunk/clusteringinspector.php
+++ trunk/clusteringinspector.php
@@ -1,9 +1,16 @@
 <?php
 @session_start();
+
+
+
 $client = $_SESSION['clientclustering'];
 if(!isset($_GET['whichDict']))
 	$whichDict = 'first';
-	else $whichDict = $_GET['whichDict'];
+	else $whichDict = $_GET['whichDict'];	
+	$localdict = $_SESSION[$_SESSION[$whichDict]]->return;
+	
+	
+	
 	
 if(!isset($_GET['threshold']))
 	$threshold = 0.75;
@@ -14,7 +21,21 @@ if(!isset($_GET['threshold']))
 		$allfeatureParameters->whichDict = $whichDict;
 		$allfeatureParameters->threshold = $threshold;
 		$allfeatureResponse = $client->getAllFeatures($allfeatureParameters);
-		echo json_encode($allfeatureResponse->return);
+		
+		$results = $allfeatureResponse->return;
+		$bound = count($results);
+		$finalresults = array();
+		
+		
+		
+		
+		for($i=0;$i<$bound;$i++)
+			if(isset($results[$i]->featureList)){
+					$finalresults[$i] = $results[$i];	
+					$finalresults[$i]->name = $localdict[$i]->name;		
+				}
+		echo json_encode($finalresults);			
+	
 	}	
 	else echo 'failed';
 ?>	
